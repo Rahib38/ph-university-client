b60b6b571d9b5338dfc000cea2072a08468807ca
Nadimul Rahib <nadimulrahib38@gmail.com>
2025-01-10 00:08:04 -0800

    Refactor hard coded admin routes

---
+++ src/routes/admin.routes.tsx
@@ -0,0 +1,78 @@
+import { ReactNode } from "react";
+import AdminDashboard from "../pages/Admin/AdminDashboard";
+import CreateAdmin from "../pages/Admin/CreateAdmin";
+import CreateFaculty from "../pages/Admin/CreateFaculty";
+import CreateStudent from "../pages/Admin/CreateStudent";
+
+type TRoutes = {
+  path: string;
+  element: ReactNode;
+};
+
+const adminPaths = [
+  {
+    name: "Dashboard",
+    path: "dashboard",
+    element: <AdminDashboard />,
+  },
+  {
+    name: "User Management",
+    children: [
+      {
+        name: "Create Student",
+        path: "create-student",
+        element: <CreateStudent />,
+      },
+      {
+        name: "Create Admin",
+        path: "create-admin",
+        element: <CreateAdmin />,
+      },
+      {
+        name: "Create Faculty",
+        path: "create-Faculty",
+        element: <CreateFaculty />,
+      },
+    ],
+  },
+];
+
+export const adminRoutes = adminPaths.reduce((acc:TRoutes[], item) => {
+  if (item.path && item.element) {
+    acc.push({
+      path: item.path,
+      element: item.element,
+    });
+  }
+  if (item.children) {
+    item.children.forEach((child) => {
+      acc.push({
+        path: child.path,
+        element: child.element,
+      });
+    });
+  }
+  return acc;
+}, []);
+
+//!hard coded
+// const adminPaths = [
+//   {
+//     path: "dashboard",
+//     element: <AdminDashboard />,
+//   },
+//   {
+//     path: "create-student",
+//     element: <CreateStudent />,
+//   },
+//   {
+//     path: "create-admin",
+//     element: <CreateAdmin />,
+//   },
+//   {
+//     path: "create-Faculty",
+//     element: <CreateFaculty />,
+//   },
+// ];
+
+export default adminPaths;
