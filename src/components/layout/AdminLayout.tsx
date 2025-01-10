09dfd9d177a082c360dfba4ad1dfc427e345318f
Nadimul Rahib <nadimulrahib38@gmail.com>
2025-01-09 20:26:51 -0800

    create admin layout

--- src/components/layout/AdminLayout.tsx
+++ src/components/layout/AdminLayout.tsx
@@ -1,8 +1,11 @@
+import { Outlet } from "react-router-dom";
 
 export default function AdminLayout() {
   return (
     <div>
+      <h1>This is nav</h1>
       <h1>This is admin layout</h1>
+      <Outlet/>
     </div>
   )
 }
