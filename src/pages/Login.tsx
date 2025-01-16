726fcc40d14805d63f1eb35119b131a2d8577deb
Nadimul Rahib <nadimulrahib38@gmail.com>
2025-01-15 20:31:35 -0800

    login page added

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,45 +1,56 @@
-import { Button } from "antd"
-import { useForm } from "react-hook-form"
-import { useLoginMutation } from "../redux/features/auth/authApi"
-import { useAppDispatch } from "../redux/features/hooks"
-import { setUser } from "../redux/features/auth/authSlice"
-import { verifyToken } from "../utils/verifyToken"
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { Button } from "antd";
+import { FieldValues, useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
+import { toast } from "sonner";
+import { useLoginMutation } from "../redux/features/auth/authApi";
+import { setUser, TUser } from "../redux/features/auth/authSlice";
+import { useAppDispatch } from "../redux/features/hooks";
+import { verifyToken } from "../utils/verifyToken";
 
 export default function Login() {
-  const dispatch=useAppDispatch()
-  const {register,handleSubmit}=useForm({
-    defaultValues:{
-      userId:"A-0001",
-      password:"admin1234"
-    }
-  })
+  const navigate = useNavigate();
+  const dispatch = useAppDispatch();
+  const { register, handleSubmit } = useForm({
+    defaultValues: {
+      userId: "A-0001",
+      password: "admin1234",
+    },
+  });
 
-  const [login,{error}]=useLoginMutation()
+  const [login] = useLoginMutation();
   // console.log('data=>',data)
   // console.log('error=>',error)
 
-  const onSubmit= async (data)=>{
-    const userInfo={
-      id:data.userId,
-      password:data.password
+  const onSubmit = async (data:FieldValues) => {
+  const toastId=  toast.loading("logging in");
+    try {
+      const userInfo = {
+        id: data.userId,
+        password: data.password,
+      };
+      const res = await login(userInfo).unwrap();
+      const user = verifyToken(res.data.accessToken) as TUser;
+      console.log(user);
+      dispatch(setUser({ user: user, token: res.data.accessToken }));
+      toast.success('Logged in',{id:toastId, duration:2000})
+      console.log(res);
+      navigate(`/${user?.role}/dashboard`);
+    } catch (err) {
+      toast.error("Something want wrong",{id:toastId,duration:2000});
     }
-const res=   await login(userInfo).unwrap()
-const user = verifyToken(res.data.accessToken)
-console.log(user)
-dispatch(setUser({user:user,token:res.data.accessToken}))
-    console.log(res)
-  }
+  };
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
       <div>
         <label htmlFor="id">ID:</label>
-        <input type="text" id="id" {...register('userId')}/>
+        <input type="text" id="id" {...register("userId")} />
       </div>
       <div>
         <label htmlFor="password">Password:</label>
-        <input type="text" id="password" {...register('password')} />
+        <input type="text" id="password" {...register("password")} />
       </div>
       <Button htmlType="submit">Log in</Button>
     </form>
-  )
+  );
 }
