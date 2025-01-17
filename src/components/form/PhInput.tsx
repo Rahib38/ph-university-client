c20cd760032a7ecf15900f2e8e8ee3cab0963965
Nadimul Rahib <nadimulrahib38@gmail.com>
2025-01-16 20:53:12 -0800

    added phInput

---
+++ src/components/form/PhInput.tsx
@@ -0,0 +1,18 @@
+import { Input } from "antd";
+import { Controller } from "react-hook-form";
+
+export default function PhInput({ type, name, label }) {
+  //   const { register } = useFormContext();
+
+  return (
+    <div style={{marginBottom:'20px'
+    }}>
+      {label ? label : null}
+
+      <Controller
+        name={name}
+        render={({ field }) => <Input {...field} type={type} id={name} />}
+      />
+    </div>
+  );
+}
