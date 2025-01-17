f713922346dd7898e46045dcd4a744f307ee3d9f
Nadimul Rahib <nadimulrahib38@gmail.com>
2025-01-16 20:53:00 -0800

    phFrom added

---
+++ src/components/form/PhFrom.tsx
@@ -0,0 +1,13 @@
+import { FormProvider, useForm } from "react-hook-form"
+
+export default function PhFrom({onSubmit,children}) {
+    const methods=useForm()
+  return (
+    <FormProvider {...methods}>
+
+    <form onSubmit={methods.handleSubmit(onSubmit)}>
+      {children}
+    </form>
+    </FormProvider>
+  )
+}
