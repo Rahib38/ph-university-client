f23cc5477060e6f1614d3b64ae2f70ea038b5283
Nadimul Rahib <nadimulrahib38@gmail.com>
2025-01-16 20:36:13 -0800

    baseApi complete

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -1,4 +1,5 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+import { BaseQueryApi, BaseQueryFn, createApi, DefinitionType, FetchArgs, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { logOut, setUser } from "../features/auth/authSlice";
 import { RootState } from "../features/store";
 const baseQuery = fetchBaseQuery({
@@ -13,7 +14,7 @@ const baseQuery = fetchBaseQuery({
   },
 });
 
-const baseQueryWithRefreshToken = async (args, api, extraOptions) => {
+const baseQueryWithRefreshToken:BaseQueryFn<FetchArgs,BaseQueryApi,DefinitionType> = async (args, api, extraOptions):Promise<any> => {
   let result = await baseQuery(args, api, extraOptions);
   if (result.error?.status === 401) {
     console.log("sending refresh token");
