d6135d133a4afc1e44b680d8cd5a2917ef43e64c
Nadimul Rahib <nadimulrahib38@gmail.com>
2025-01-20 01:09:47 -0800

    update

--- src/pages/Admin/academicManagement/AcademicFaculty.tsx
+++ src/pages/Admin/academicManagement/AcademicFaculty.tsx
@@ -1,8 +1,52 @@
+import { Table, TableColumnsType } from "antd";
+import { useGetAllAcademicFacultyQuery } from "../../../redux/features/admin/academicManagement.api";
+export type TTableDatas = {
+  name: string;
+};
 const AcademicFaculty = () => {
+  const {
+    data: semestersData,
+    isLoading,
+    isFetching,
+  } = useGetAllAcademicFacultyQuery(undefined);
+  const tableData = semestersData?.data?.map((item, id) => ({
+    key: id,
+    name: item?.name,
+  }));
+  const columns: TableColumnsType<TTableDatas> = [
+    {
+      title: "Faculty Name",
+      key: "facultyName",
+      dataIndex: "name",
+      // showSorterTooltip: { target: "full-header" },
+
+      // specify the condition of filtering result
+      // here is that finding the name started with `value`
+      // onFilter: (value, record) => record.name.indexOf(value as string) === 0,
+      // sorter: (a, b) => a.name.length - b.name.length,
+      // sortDirections: ["descend"],
+    },
+  ];
+  // const onChange: TableProps<TTableData>["onChange"] = (
+  //   pagination,
+  //   filters,
+  //   sorter,
+  //   extra
+  // ) => {
+  //   console.log("params", pagination, filters, sorter, extra);
+  // };
+
+  if (isLoading) {
+    return <p>Loading</p>;
+  }
   return (
-    <div>
-      <h1>Academic Faculty</h1>
-    </div>
+    <Table
+      loading={isFetching}
+      columns={columns}
+      dataSource={tableData}
+      // onChange={onChange}
+      showSorterTooltip={{ target: "sorter-icon" }}
+    />
   );
 };
 
