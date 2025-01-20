66968881419f89310a394b5f3515b2ae777f050e
Nadimul Rahib <nadimulrahib38@gmail.com>
2025-01-19 22:32:54 -0800

    update

--- src/components/form/PhSelect.tsx
+++ src/components/form/PhSelect.tsx
@@ -3,9 +3,10 @@ import { Controller } from "react-hook-form";
 type TPHSelectProps = {
   label: string;
   name: string;
-  options:{value:string,label:string,disable?:boolean}[]
+  options:{value:string,label:string,disable?:boolean}[] | undefined
+  disabled?:boolean
 };
-const PhSelect = ({ label, name ,options}: TPHSelectProps) => {
+const PhSelect = ({ label, name ,options,disabled}: TPHSelectProps) => {
   // const handleChange=(value:string)=>{
   //     console.log(`selected ${value}`)
   // }
@@ -19,6 +20,7 @@ const PhSelect = ({ label, name ,options}: TPHSelectProps) => {
             {...field}
             options={options}
             size="large"
+            disabled={disabled}
           />
           {error &&<small style={{color:'red'}}>{error.message}</small>}
         </Form.Item>
