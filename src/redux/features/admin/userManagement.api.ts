71aa16d67d1d2b114032c9f0f2b12964e5c1175a
Nadimul Rahib <nadimulrahib38@gmail.com>
2025-01-20 21:56:13 -0800

    update

--- src/redux/features/admin/userManagement.api.ts
+++ src/redux/features/admin/userManagement.api.ts
@@ -1,29 +1,30 @@
+import { TQueryParam, TResponseRedux, TStudent } from "../../../types";
 import { baseApi } from "../../api/baseApi";
 
 const userManagementApi = baseApi.injectEndpoints({
   endpoints: (builder) => ({
-    // getAllSemesters: builder.query({
-    //   query: (args) => {
-    //     const params = new URLSearchParams();
-    //     if (args) {
-    //       args.forEach((item: TQueryParam) => {
-    //         params.append(item.name, item.value as string);
-    //       });
-    //     }
-    //     return {
-    //       url: "/academic-semesters",
-    //       method: "GET",
-    //       params: params,
-    //     };
-    //   },
-    //   transformResponse: (response: TResponseRedux<TAcademicSemester[]>) => {
-    //     console.log("inside redux", response);
-    //     return {
-    //       data: response.data,
-    //       meta: response.meta,
-    //     };
-    //   },
-    // }),
+    getAllStudents: builder.query({
+      query: (args) => {
+        const params = new URLSearchParams();
+        if (args) {
+          args.forEach((item: TQueryParam) => {
+            params.append(item.name, item.value as string);
+          });
+        }
+        return {
+          url: "/students",
+          method: "GET",
+          params: params,
+        };
+      },
+      transformResponse: (response: TResponseRedux<TStudent[]>) => {
+        console.log("inside redux", response);
+        return {
+          data: response.data,
+          meta: response.meta,
+        };
+      },
+    }),
     addStudent: builder.mutation({
       query: (data) => ({
         url: "/users/create-student",
@@ -33,4 +34,4 @@ const userManagementApi = baseApi.injectEndpoints({
     }),
   }),
 });
-export const {useAddStudentMutation}=userManagementApi
+export const { useAddStudentMutation,useGetAllStudentsQuery } = userManagementApi;
