feb6db9c9b11061687b5edd93395b3aea081505e
Nadimul Rahib <nadimulrahib38@gmail.com>
2025-01-21 00:06:57 -0800

    studentData

--- src/pages/Admin/userManagement/StudentData.tsx
+++ src/pages/Admin/userManagement/StudentData.tsx
@@ -9,13 +9,14 @@ import {
 import { useState } from "react";
 import { useGetAllStudentsQuery } from "../../../redux/features/admin/userManagement.api";
 import { TQueryParam, TStudent } from "../../../types";
+import { Link } from "react-router-dom";
 // interface DataType {
 //   key: React.Key;
 //   name: string;
 //   age: number;
 //   address: string;
 // }
-export type TTableData = Pick<TStudent, "fullName" | "id">;
+export type TTableData = Pick<TStudent, "fullName" | "id" |'email'|"contactNo" >;
 const StudentData = () => {
   const [params, setParams] = useState<TQueryParam[]>([]);
   const [page, setPage] = useState(1);
@@ -24,16 +25,16 @@ const StudentData = () => {
     isLoading,
     isFetching,
   } = useGetAllStudentsQuery([
-    { name: "limit", value: 1 },
+    { name: "limit", value: 3 },
     { name: "page", value: page },
     { name: "sort", value: "id" },
     ...params,
   ]);
   const metaData = studentData?.meta;
-  const tableData = studentData?.data?.map(({ _id, fullName, id }) => ({
+  const tableData = studentData?.data?.map(({ _id, fullName, id,email,contactNo }) => ({
     key: _id,
     fullName,
-    id,
+    id,email,contactNo
   }));
   const columns: TableColumnsType<TTableData> = [
     {
@@ -49,15 +50,27 @@ const StudentData = () => {
       key: "id",
       dataIndex: "id",
     },
+    {
+      title: "Email",
+      key: "email",
+      dataIndex: "email",
+    },
+    {
+      title: "Contact No",
+      key: "contactNo",
+      dataIndex: "contactNo",
+    },
 
     {
       title: "Action",
       key: "x",
-      render: () => {
+      render: (item) => {
+        console.log(item)
         return (
           <Space>
-            <Button>update</Button>
-            <Button>Details</Button>
+           <Link to={`/admin/student-data/${item?.key}`}>    <Button>Details</Button></Link>
+           <Button>update</Button>
+        
             <Button>Block</Button>
           </Space>
         );
